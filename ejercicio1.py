d1116e490749d4f7268392a062bd3e267def1e8d
Enmauel <134546520+DarkShadowLOL10@users.noreply.github.com>
2024-03-28 22:39:59 -0300

    ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,9 +1,13 @@
+# ingresamos el nombre del usuario #
 usuario = str(input("indique su nombre: "))
 
-dia_consumo1 = float(input("comida consumida del dia 1 de su familia:  "))
-dia_consumo2 = float(input("comida consumida del dia 2 de su familia:  "))
-dia_consumo3 = float(input("comida consumida del dia 3 de su familia:  "))
+# ingresamos los consumos diarios #
+Dia1 = float(input("comida consumida del dia 1 de su familia:  "))
+Dia2 = float(input("comida consumida del dia 2 de su familia:  "))
+Dia3 = float(input("comida consumida del dia 3 de su familia:  "))
 
-promedio_del_consumo =  (dia_consumo1 + dia_consumo2 + dia_consumo3)/3
+# Calcular el promedio de consumos #
+promedio =  (Dia1 + Dia2 + Dia3)/3
 
-print ("Estimado/a " + usuario+ " Su promedio es: " + str(promedio_del_consumo) + " del consumo de su familia del dia.")
+# Mostramos el msj por pantalla #
+print ("Estimado/a " + str(usuario) + " Su promedio es: " + str(promedio) + " del consumo de su familia del dia.")
